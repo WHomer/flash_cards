9009a4c4ed0bdcc28e27d588b75240dbc937d6fc
William Homer <billyhomeriii@gmail.com>
2019-03-12 12:58:49 -0600

    Complete card_test

--- test/card_test.rb
+++ test/card_test.rb
@@ -1,15 +1,9 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
-require './lib/turn'
+
 
 class CardTest < Minitest::Test
-  def turn_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_instance_of Turn, turn
-  end
 
   def test_it_exists
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
